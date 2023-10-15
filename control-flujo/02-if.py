8315f9c36122fb3ed38afa22a79d1878e896be34
Pere Sabates <peresabates@yahoo.es>
2023-10-15 11:33:54 +0200

    first commit

---
+++ control-flujo/02-if.py
@@ -0,0 +1,10 @@
+edad = 15
+if edad > 54:
+    print("Puedes ver la película con descuento")
+elif edad > 17:
+    print("Puedes ver la película")
+else:
+    print("No puedes entrar")
+    print("Ve a otro lado")
+
+print("Listo")
