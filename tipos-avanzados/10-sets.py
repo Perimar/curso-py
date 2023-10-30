93c864461a9ad4cd5b750a10dbaefd3d8418b314
Perimar <146140081+Perimar@users.noreply.github.com>
2023-10-30 19:50:48 +0100

    main

--- tipos-avanzados/10-sets.py
+++ tipos-avanzados/10-sets.py
@@ -1,3 +1,9 @@
 # set significa grupo o conjunto
 primer = {1, 1, 2, 2, 3, 4}
-print(primer)
+segundo = [3, 4, 5]
+segundo = set(segundo)
+
+print(primer | segundo)
+print(primer & segundo)
+print(primer - segundo)
+print(primer ^ segundo)
